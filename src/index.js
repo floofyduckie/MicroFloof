3e1f4490aaf7ad9759b57769a6456b8fa55717a3
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-11-06 15:30:54 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+
+require('dotenv').config();
+const {Client, IntentsBitField, messageLink} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+
+    ],
+});
+
+client.on('ready',(c) => {
+    console.log(`☑️ ${c.user.tag} is online`);
+});  
+
+client.on('messageCreate', (message) => {
+
+    if (message.author.bot){
+        return;
+    }
+
+    if (message.content === "hello"){
+        message.reply("hello!");
+
+    }
+
+    // console.log(message.content);
+    // console.log(message.author.username);
+});
+
+client.login(process.env.TOKEN);
