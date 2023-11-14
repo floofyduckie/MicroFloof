e6c4193854cdd5357454581dbbadfbab1ea09537
Aditya Agarwal <adityaagarwal191@gmail.com>
2023-11-14 23:57:41 +0000

    Set up embeds

--- src/index.js
+++ src/index.js
@@ -1,32 +1,91 @@
-require('dotenv').config();
-const {Client, IntentsBitField, messageLink} = require('discord.js');
+require("dotenv").config();
+const {
+  Client,
+  IntentsBitField,
+  messageLink,
+  EmbedBuilder,
+} = require("discord.js");
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-
-    ],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-client.on('ready',(c) => {
-    console.log(`☑️ ${c.user.tag} is online`);
-});  
+client.on("ready", (c) => {
+  console.log(`☑️ ${c.user.tag} is online`);
+});
 
-client.on('interactionCreate',(interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-    // console.log(interaction.commandName);
+  // console.log(interaction.commandName);
 
-    if (interaction.commandName === "hey"){
-        interaction.reply("hey back!");
-    }
+  if (interaction.commandName === "hey") {
+    interaction.reply("hey back!");
+  }
 
-    if (interaction.commandName === "ping"){
-        interaction.reply("Pong!");
-    }
+  if (interaction.commandName === "ping") {
+    interaction.reply("Pong!");
+  }
+
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.get("first-number").value;
+    const num2 = interaction.options.get("second-number").value;
+
+    interaction.reply(`The sum is ${num1 + num2}`);
+  }
+
+  if (interaction.commandName === "embed") {
+    const embed = new EmbedBuilder()
+      .setTitle("Embed Title")
+      .setDescription("This is the description for the embed")
+      .setColor("Red")
+      .addFields(
+        {
+          name: "Field Title",
+          value: "This is value for the field",
+          inline: true,
+        },
+
+        {
+          name: "Second Field Title",
+          value: "This is value for the second field",
+          inline: true,
+        }
+      )
+      .setTimestamp();
+
+    interaction.reply({ embeds: [embed] });
+  }
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "embed") {
+    const embed = new EmbedBuilder()
+      .setTitle("Embed Title")
+      .setDescription("This is the description for the embed")
+      .setColor("Red")
+      .addFields(
+        {
+          name: "Field Title",
+          value: "This is value for the field",
+          inline: true,
+        },
+
+        {
+          name: "Second Field Title",
+          value: "This is value for the second field",
+          inline: true,
+        }
+      )
+      .setTimestamp();
+
+	message.channel.send({embeds: [embed]});
+  }
 });
 
 // client.on('messageCreate', (message) => {
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
-const {REST, Routes} = require('discord.js');
+const {REST, Routes, ApplicationCommandOptionType } = require('discord.js');
 
 const commands = [
+    {
+
+        name: 'embed',
+        description: 'Sends an embed!',
+
+    },
+
+
     {
         name: 'hey',
         description: 'Replies with hey',
@@ -10,6 +18,53 @@ const commands = [
     {
         name: 'ping',
         description: 'Replies with pong',
+    },
+
+    {
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number',
+                type: ApplicationCommandOptionType.Number,
+                choices:[
+                    {
+                        name: 'one',
+                        value: 1,
+                    },
+                    {
+                        name: 'two',
+                        value: 2,
+                    },
+                    {
+                        name: 'three',
+                        value: 3,
+                    },
+                ],
+                required: true,
+            },
+            {
+                name: 'second-number',
+                description: 'The second number',
+                type: ApplicationCommandOptionType.Number,
+                choices:[
+                    {
+                        name: 'one',
+                        value: 1,
+                    },
+                    {
+                        name: 'two',
+                        value: 2,
+                    },
+                    {
+                        name: 'three',
+                        value: 3,
+                    },
+                ],
+                required:true,
+            }
+        ]
     },
 ];
 
